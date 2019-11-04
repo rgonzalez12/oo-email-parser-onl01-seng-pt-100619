45e157e1c552a7e2dd0c0353f50c6c1604afe3de
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-11-04 02:19:31 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,8 +13,8 @@ class EmailAddressParser
   def parse
     array = []
     array << @email_addresses
-    @email_addresses.each do |email_address|
-      email_address.split(/, ?/)
+    @email_addresses.split(/, ?/)each do |email_address|
+      
       
       
     end
